8df7babbd51c9772212a4554054658abc997d559
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2022-06-15 00:41:18 +0200

    refactoring header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -11,67 +11,70 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classes from "../../assets/scss/Header.module.scss";
 
 const Header = (props) => {
+   const inputContainer = (
+      <div className={classes.input_container}>
+         <Select className={classes.select} />
+         <Input
+            className={classes.search_input}
+            input={{ id: "search", type: "text", placeholder: "Cerca" }}
+         />
+         <div className={classes.search_icon_container}>
+            <FontAwesomeIcon className={classes.search_icon} icon="fa-search" />
+         </div>
+      </div>
+   );
+
+   const cart = (
+      <button
+         className={classes.container_with_icon}
+         onClick={props.onShowCart}
+      >
+         <FontAwesomeIcon
+            icon="fa-cart-shopping"
+            className={classes.cart_icon}
+         />
+         <DoubleLine lineOne={<br></br>} lineTwo={"Cart"} />
+      </button>
+   );
+
    return (
-      <Fragment>
-         <header className={classes.header}>
-            <img className={classes.logo} src={amazon_logo} alt="Amazon logo" />
+      <header className={classes.header}>
+         <img className={classes.logo} src={amazon_logo} alt="Amazon logo" />
 
-            <div
-               className={`${classes.container_with_icon} d_none_w_md d_none_w_s`}
-            >
-               <FontAwesomeIcon
-                  icon="fa-location-dot"
-                  className={classes.location_icon}
-               />
-               <DoubleLine lineOne={"Deliver to Davide"} lineTwo={"Italy"} />
-            </div>
+         <div
+            className={`${classes.container_with_icon} d_none_w_md d_none_w_s`}
+         >
+            <FontAwesomeIcon
+               icon="fa-location-dot"
+               className={classes.location_icon}
+            />
+            <DoubleLine lineOne={"Deliver to Davide"} lineTwo={"Italy"} />
+         </div>
 
-            <div className={classes.input_container}>
-               <Select className={classes.select} />
-               <Input
-                  className={classes.search_input}
-                  input={{ id: "search", type: "text", placeholder: "Cerca" }}
-               />
-               <div className={classes.search_icon_container}>
+         {inputContainer}
+
+         <DoubleLine
+            className="d_none_w_md"
+            lineOne={"Hello, Sing in"}
+            lineTwo={
+               <Fragment>
+                  Account & Lists
                   <FontAwesomeIcon
-                     className={classes.search_icon}
-                     icon="fa-search"
+                     className={classes.caret_down_icon}
+                     icon="fa-solid fa-caret-down"
                   />
-               </div>
-            </div>
+               </Fragment>
+            }
+         />
 
-            <DoubleLine
-               className="d_none_w_md"
-               lineOne={"Hello, Sing in"}
-               lineTwo={
-                  <Fragment>
-                     Account & Lists
-                     <FontAwesomeIcon
-                        className={classes.caret_down_icon}
-                        icon="fa-solid fa-caret-down"
-                     />
-                  </Fragment>
-               }
-            />
+         <DoubleLine
+            className="d_none_w_md"
+            lineOne={"Returns"}
+            lineTwo={"& Orders"}
+         />
 
-            <DoubleLine
-               className="d_none_w_md"
-               lineOne={"Returns"}
-               lineTwo={"& Orders"}
-            />
-
-            <button
-               className={classes.container_with_icon}
-               onClick={props.onShowCart}
-            >
-               <FontAwesomeIcon
-                  icon="fa-cart-shopping"
-                  className={classes.cart_icon}
-               />
-               <DoubleLine lineOne={<br></br>} lineTwo={"Cart"} />
-            </button>
-         </header>
-      </Fragment>
+         {cart}
+      </header>
    );
 };
 
