891b0067df292cb11b7c85d2dd1be4d36ad4e9b6
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2022-06-16 15:08:26 +0200

    refactoring footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -9,12 +9,11 @@ import {
    listItemsCol4,
 } from "../../assets/config/footerListItems";
 import languages from "../../assets/config/selectLanguages";
-
 import amazon_logo from "../../assets/images/amazon-logo-white.png";
 
 import classes from "../../assets/scss/Footer.module.scss";
 
-const Footer = (props) => {
+const Footer = () => {
    const lists = (
       <Fragment>
          <div className={classes.col}>
@@ -32,6 +31,25 @@ const Footer = (props) => {
       </Fragment>
    );
 
+   const footerBottom = (
+      <Fragment>
+         <a href="https://amazon.com">
+            <img src={amazon_logo} alt="logo amazon" />
+         </a>
+         <div className={classes.container_el_footer}>
+            <div className={classes.footer_b_el}>
+               <Select
+                  className={classes.select}
+                  defaultValue="English"
+                  values={languages}
+               />
+            </div>
+            <div className={classes.footer_b_el}>USD - U.S. Dollar</div>
+            <div className={classes.footer_b_el}>United States</div>
+         </div>
+      </Fragment>
+   );
+
    return (
       <footer className={classes.footer}>
          <a href="#header">
@@ -40,22 +58,7 @@ const Footer = (props) => {
 
          <div className={classes.footer_middle}>{lists}</div>
 
-         <div className={classes.footer_bottom}>
-            <a href="https://amazon.com">
-               <img src={amazon_logo} alt="logo amazon" />
-            </a>
-            <div className={classes.container_el_footer}>
-               <div className={classes.footer_b_el}>
-                  <Select
-                     className={classes.select}
-                     defaultValue="English"
-                     values={languages}
-                  />
-               </div>
-               <div className={classes.footer_b_el}>USD - U.S. Dollar</div>
-               <div className={classes.footer_b_el}>United States</div>
-            </div>
-         </div>
+         <div className={classes.footer_bottom}>{footerBottom}</div>
       </footer>
    );
 };
