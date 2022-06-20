67005b9f7f8f924182b6133805f75928e27086bc
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2022-06-20 13:09:14 +0200

    add favicon

--- src/components/Layout/Intro.js
+++ src/components/Layout/Intro.js
@@ -6,7 +6,7 @@ const Intro = () => {
    return (
       <section className={classes.section} id="intro">
          <div className={classes.heading}>
-            TEST Amazon home page's clone made with{" "}
+            Amazon home page's clone made with{" "}
             <span>
                <img src={react_logo} alt="Logo React" />
                React
